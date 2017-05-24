8d352148cf34fade12c67e293c1a11d9f697014f
luismachado <luismachado5@gmail.com>
2017-05-24 16:15:48 +0100

    base controller structure

---
+++ Bliss Recruitment App/QuestionDetailController.swift
@@ -0,0 +1,20 @@
+//
+//  QuestionDetailController.swift
+//  Bliss Recruitment App
+//
+//  Created by Luís Machado on 24/05/17.
+//  Copyright © 2017 LuisMachado. All rights reserved.
+//
+
+import UIKit
+
+class QuestionDetailController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+    }
+    
+}
---
+++ Bliss Recruitment App/QuestionListController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Bliss Recruitment App
+//
+//  Created by Luís Machado on 24/05/17.
+//  Copyright © 2017 LuisMachado. All rights reserved.
+//
+
+import UIKit
+
+class QuestionListController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    let questionCellId = "questionCellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Bliss Recruitment App"
+        collectionView?.backgroundColor = .yellow
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: questionCellId)
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: questionCellId, for: indexPath)
+        cell.backgroundColor = .blue
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: self.view.frame.width, height: 100)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("clicked")
+        
+        let questionDetailController = QuestionDetailController()
+        navigationController?.pushViewController(questionDetailController, animated: true)
+    }
+}
+
